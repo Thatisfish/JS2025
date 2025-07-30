95ccdb398cd9c565d1dcbf9253fe32faf7bdbf01
Thatisfish <roobin4321@gmail.com>
2025-07-30 16:53:46 +0800

    0730

---
+++ YMPM/JS/main.js
@@ -0,0 +1,10 @@
+let navigation = document.querySelector('.navigation');
+
+window, addEventListener('scroll', () => {
+    // console.log(window.pageYOffset);
+    if (window.pageYOffset != 0) {
+        navigation.style.backgroundColor = "#000" 
+    } else{
+        navigation.style.backgroundColor = "rgba(255, 255, 255, 0)"
+    }
+})
