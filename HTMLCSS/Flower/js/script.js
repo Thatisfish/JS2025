2c30b8949c529adf1de3ef8e80196122f41c684f
Thatisfish <roobin4321@gmail.com>
2025-08-28 17:01:58 +0800

    0828

--- HTMLCSS/Flower/js/script.js
+++ HTMLCSS/Flower/js/script.js
@@ -1,18 +1,26 @@
 $(function () {
-    $(".menu a").click(function () {
-        var btn = $(this).attr("href");
-        var pos = $(btn).offset();
-        $("html,body").animate({ scrollTop: pos.top }, 1000);
-    });
-    $("#gotop").click(function () {
-        $("html,body").animate({ scrollTop: 0 }, 1000);
-    });
+	$('.hambuger').click(function () {
+		$(this).toggleClass('is-active');
+		$('.navigation').toggleClass("show");
+	})
+	$(".menu a").click(function () {
+		var btn = $(this).attr("href");
+		var pos = $(btn).offset();
+		$("html,body").animate({ scrollTop: pos.top }, 1000);
+	});
+	$("#gotop").click(function () {
+		$("html,body").animate({ scrollTop: 0 }, 1000);
+	});
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $("#gotop").stop().fadeIn("fast",1);
-        } else {
-            $("#gotop").stop().fadeOut("fast");
-        }
-    });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$("#gotop").stop().fadeIn("fast", 1);
+		} else {
+			$("#gotop").stop().fadeOut("fast");
+		}
+	});
+});
+
+$('.smoove').smoove({
+	offest: 250
 });
